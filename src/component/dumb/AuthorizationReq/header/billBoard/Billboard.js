7d878a4a1ddecd6129c2e921288643f6863e8a03
Syed Asrar Zahoor <81914950+Syed-Asrar@users.noreply.github.com>
2023-05-14 21:10:47 +0530

    top 10 added

--- src/component/dumb/AuthorizationReq/header/billBoard/Billboard.js
+++ src/component/dumb/AuthorizationReq/header/billBoard/Billboard.js
@@ -7,6 +7,10 @@ export default function Billboard({ children, ...props}) {
   return (
     <div className='billboard-container' {...props}>
         <img src='images/home/bg-img-title.png' alt='' className='billboard-title-img'/>
+        <div className='top-10-container'>
+          <img src='images/general/top10.png' alt='top 10' className='top-10-icon'/>
+          <p className='top-rank'>#3 in TV Shows Today</p>
+        </div>
         <p className='billboard-desc'>{children}</p>
         <div className='btn-container'>
           <button className='play-button'><BsFillPlayFill className='play-btn-icon' />Play</button>
