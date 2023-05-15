42b13e5681ab1dff64589154cbf00190e420cda1
Syed Asrar Zahoor <81914950+Syed-Asrar@users.noreply.github.com>
2023-05-15 17:44:07 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import SignHomepage from './pages/SignHomepage';
 import SignInPage from './pages/Sign';
 import Homepage from './pages/Homepage'
@@ -11,12 +11,13 @@ import Homepage from './pages/Homepage'
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route path="/" element={<SignHomepage />} />
-        <Route path="/login" element={<SignInPage />} />
-        <Route path="/browse" element={<Homepage />} />
-      </Routes> */}
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignHomepage />} />
+          <Route path="/login" element={<SignInPage />} />
+          <Route path="/browse" element={<Homepage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
